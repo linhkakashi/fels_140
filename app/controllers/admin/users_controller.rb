8fcf0bb3ced0f00785d04090b015fa5326746c6c
linhtn_57 <hotdog160993@gmail.com>
2016-06-09 10:46:10 +0700

    Edit and show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,38 @@
 class Admin::UsersController < ApplicationController
   before_action :logged_in_user, :verify_user
+  before_action :load_user, except: :index
 
   def index
     @users = User.paginate page: params[:page]
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "notice.profile_update"
+      redirect_to admin_user_path @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = t "notice.delete_success"
+    redirect_to admin_users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation, :avatar
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
   end
 end
