2a0f1aebe4d2a1ef7adbc6f3a643d881fb12a0f3
linhtn_57 <hotdog160993@gmail.com>
2016-06-10 10:04:11 +0700

    Following user

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,4 +48,20 @@ class User < ActiveRecord::Base
   def forget
     update_attributes remember_digest: nil
   end
+
+  def follow other_user
+    active_relationships.create followed_id: other_user.id
+  end
+
+  def unfollow other_user
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following? other_user
+    following.include? other_user
+  end
+
+  def is_user? user
+    self == user
+  end
 end
