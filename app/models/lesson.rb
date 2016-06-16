4eaacc53a5e0ce5604759067be8209032bbac948
linhtn_57 <hotdog160993@gmail.com>
2016-06-16 13:12:26 +0700

    Lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,12 +3,21 @@ class Lesson < ActiveRecord::Base
 
   belongs_to :category
   belongs_to :user
-  has_many :lesson_words
+  has_many :lesson_words, dependent: :destroy
+  has_many :words, through: :lesson_words
+
+  accepts_nested_attributes_for :lesson_words
 
   after_create :create_lesson_activity
+  before_create :create_words_lesson
 
   private
   def create_lesson_activity
     create_activity self.user_id, self.id, Settings["start_lesson"]
+  end
+
+  def create_words_lesson
+    word_ids = category.words.order("RANDOM()").limit(Settings["QUESTION_NUMBERS"]).map(&:id)
+    self.word_ids = word_ids
   end
 end
